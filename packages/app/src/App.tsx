631a04377bc42d684e47cfff4e523464355c8ba3
Peter Macdonald <macdonald.peter90@gmail.com>
2023-04-28 15:00:44 +0200

    OwnedGroupsField
    Signed-off-by: Peter Macdonald <macdonald.peter90@gmail.com>

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -42,13 +42,15 @@ const app = createApp({
     SignInPage: props => (
       <SignInPage
         {...props}
-        auto
-        provider={{
-          id: 'github-auth-provider',
-          title: 'GitHub',
-          message: 'Sign in using GitHub',
-          apiRef: githubAuthApiRef,
-        }}
+        providers={[
+          'guest',
+          {
+            id: 'github-auth-provider',
+            title: 'GitHub',
+            message: 'Sign in using GitHub',
+            apiRef: githubAuthApiRef,
+          },
+        ]}
       />
     ),
   },
--- plugins/scaffolder-frontend-module-ownedgroups-field/src/components/OwnedGroupsPicker/OwnedGroupsPicker.tsx
+++ plugins/scaffolder-frontend-module-ownedgroups-field/src/components/OwnedGroupsPicker/OwnedGroupsPicker.tsx
@@ -1,38 +1,38 @@
-/* eslint-disable @backstage/no-undeclared-imports */
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useApi, identityApiRef } from '@backstage/core-plugin-api';
-import { TextField } from '@material-ui/core';
+import { FormControl, InputLabel, MenuItem, Select } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { catalogApiRef } from '@backstage/plugin-catalog-react';
 import { NotFoundError } from '@backstage/errors';
-import { Entity } from '@backstage/catalog-model';
+import { GroupEntity } from '../../types';
 
 
-const useStyles = makeStyles({
-  dropdown: {
+const useStyles = makeStyles(() => ({
+  formControl: {
     minWidth: 120,
   },
-});
+}));
 
 export const OwnedGroupsPicker = () => {
   const classes = useStyles();
   const identityApi = useApi(identityApiRef);
   const catalogApi = useApi(catalogApiRef);
-  const [groups, setGroups] = useState<Entity[]>([]);
+  const [groups, setGroups] = useState<GroupEntity[]>([]);
+  const [selectedGroup, setSelectedGroup] = useState('');
 
   useEffect(() => {
     const fetchUserGroups = async () => {
-      const backstageIdentity = await identityApi.getBackstageIdentity();
-      const ownedEntities = backstageIdentity?.ownershipEntityRefs;
-      
-      if (!ownedEntities) {
-        throw new NotFoundError(`Could not find owned entities for user ${ownedEntities}`);
+      const identity = await identityApi.getBackstageIdentity();
+      const userIdentity = identity.ownershipEntityRefs;
+
+      if (!userIdentity) {
+        throw new NotFoundError(`Could not find owned entities for user ${userIdentity}`);
       }
 
       const userOwnedGroups = await catalogApi.getEntities({
         filter: {
           kind: 'Group',
-          'metadata.owners': ownedEntities,
+          'relations.hasMember': userIdentity
         },
       });
       setGroups(userOwnedGroups.items);
@@ -41,18 +41,32 @@ export const OwnedGroupsPicker = () => {
     fetchUserGroups();
   }, [identityApi, catalogApi]);
 
+  const handleChange = useCallback((event: React.ChangeEvent<{ value: unknown }>) => {
+    setSelectedGroup(event.target.value as string);
+  }, []);
+
   return (
-    <TextField
-      select
-      label="User Owned Groups"
-      className={classes.dropdown}
-      variant="outlined"
-    >
-      {groups.map((group: any) => (
-        <option key={group.metadata.name} value={group.metadata.name}>
-          {group.metadata.name}
-        </option>
-      ))}
-    </TextField>
+    <FormControl variant="outlined" className={classes.formControl}>
+      <InputLabel id="user-owned-groups-label">User Owned Groups</InputLabel>
+      <Select
+        labelId="user-owned-groups-label"
+        id="user-owned-groups-select"
+        value={selectedGroup}
+        onChange={handleChange}
+        label="User Owned Groups"
+      >
+        {groups.length === 0 ? (
+          <MenuItem value="" disabled>
+            No Matching Groups
+          </MenuItem>
+        ) : (
+          groups.map((group: GroupEntity) => (
+            <MenuItem key={group.metadata.name} value={group.metadata.name}>
+              {group.metadata.name}
+            </MenuItem>
+          ))
+        )}
+      </Select>
+    </FormControl>
   );
 };
