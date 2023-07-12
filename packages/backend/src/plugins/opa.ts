3ef7c2c8dfd1b84681330619e6b2df157d7961b4
Parsifal-M <macdonald.peter90@gmail.com>
2023-07-12 16:19:37 +0200

    Readme update
    Signed-off-by: Parsifal-M <macdonald.peter90@gmail.com>

--- packages/backend/src/plugins/opa.ts
+++ packages/backend/src/plugins/opa.ts
@@ -5,11 +5,7 @@ import { PluginEnvironment } from '../types';
 export default async function createPlugin(
   env: PluginEnvironment,
 ): Promise<Router> {
-  // Here is where you will add all of the required initialization code that
-  // your backend plugin needs to be able to start!
 
-  // The env contains a lot of goodies, but our router currently only
-  // needs a logger
   return await createRouter({
     logger: env.logger,
     config: env.config,
