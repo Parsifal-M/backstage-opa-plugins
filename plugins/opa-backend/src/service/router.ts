a1a73cbb40f2caaad321a1f9ee2e9d2c626e6100
Parsifal-M <macdonald.peter90@gmail.com>
2023-08-03 17:18:55 +0200

    OPA-Work
    Signed-off-by: Parsifal-M <macdonald.peter90@gmail.com>

--- plugins/opa-backend/src/service/router.ts
+++ plugins/opa-backend/src/service/router.ts
@@ -19,22 +19,48 @@ export async function createRouter(
   const router = Router();
   router.use(express.json());
 
-  router.post('/entity-checker', async (request, response) => {
-    const entityMetadata = request.body.input;
-    const packageName = config.getString(
-      'opa-client.opa.policies.entityChecker.package',
-    );
-    const opaUrl = `http://localhost:8181/v1/data/${packageName}/`;
+  // Get the config options for the OPA plugin
+  const opaAddr = config.getOptionalString('opa-client.opa.baseUrl');
+
+  // Get Packages
+  const entityCheckerPackage = config.getOptionalString("opa-client.opa.policies.entityChecker.package");
+  const catalogPermissionPackage = config.getOptionalString("opa-client.opa.policies.catalogPermission.package");
+
+  router.get('/health', (_, resp) => {
+    resp.json({ status: 'ok' });
+  });
+
+  router.post('/entity-checker', async (req, res) => {
+    const entityMetadata = req.body.input;
+    const opaUrl = `${opaAddr}/v1/data/${entityCheckerPackage}/`;
 
     try {
       const opaResponse = await axios.post(opaUrl, {
         input: entityMetadata,
       });
 
-      response.json(opaResponse.data.result);
+      res.json(opaResponse.data.result);
     } catch (error) {
       logger.error('Failed to evaluate metadata with OPA', error);
-      response.status(500).send('Failed to evaluate metadata with OPA');
+      res.status(500).send('Failed to evaluate metadata with OPA');
+    }
+  });
+
+  router.post('/catalog-permission', async (req, res) => {
+    const catalogPolicyInput = req.body.input;
+    const entityData = req.body.entityData;
+    const opaUrl = `${opaAddr}/v1/data/${catalogPermissionPackage}/`;
+
+    try {
+      const opaResponse = await axios.post(opaUrl, {
+        input: catalogPolicyInput,
+        data: entityData,
+      });
+      
+      res.json(opaResponse.data.result);
+    } catch (error) {
+      logger.error('Failed to evaluate permission data with OPA', error);
+      res.status(500).send('Failed to evaluate permission data with OPA');
     }
   });
 
--- plugins/opa-wrapper/src/opa-evaluator/opaPermissionEvaluator.ts
+++ plugins/opa-wrapper/src/opa-evaluator/opaPermissionEvaluator.ts
@@ -1,48 +1,47 @@
 import { PolicyDecision, isResourcePermission, AuthorizeResult } from "@backstage/plugin-permission-common";
 import { PolicyQuery } from "@backstage/plugin-permission-node";
-import { PolicyEvaluationInput, PolicyEvaluationResult } from "../../types";
+import { PolicyEvaluationInput } from "../../types";
 import { OpaClient } from "../opa-client/opaClient";
 // eslint-disable-next-line @backstage/no-undeclared-imports
 import { BackstageIdentityResponse } from '@backstage/plugin-auth-node';
 
 export function createOpaPermissionEvaluator(opaClient: OpaClient) {
-    return async (
-      request: PolicyQuery,
-      user?: BackstageIdentityResponse,
-    ): Promise<PolicyDecision> => {
-      const resourceType = isResourcePermission(request.permission)
-        ? request.permission.resourceType
-        : undefined;
-      const userGroups = user?.identity.ownershipEntityRefs ?? [];
-      const userName = user?.identity.userEntityRef;
-      const {
-        type,
-        name,
-        attributes: { action },
-      } = request.permission;
-  
-      const input: PolicyEvaluationInput = {
-        input: {
-          permission: {
-            type,
-            name,
-            action,
-            resourceType,
-          },
-          identity: {
-            username: userName,
-            groups: userGroups,
-          },
+  return async (
+    request: PolicyQuery,
+    user?: BackstageIdentityResponse,
+  ): Promise<PolicyDecision> => {
+    const resourceType = isResourcePermission(request.permission)
+      ? request.permission.resourceType
+      : undefined;
+    const userGroups = user?.identity.ownershipEntityRefs ?? [];
+    const userName = user?.identity.userEntityRef;
+    const {
+      type,
+      name,
+      attributes: { action },
+    } = request.permission;
+
+    const input: PolicyEvaluationInput = {
+      input: {
+        permission: {
+          type,
+          name,
+          action,
+          resourceType,
         },
-      };
-  
-      const evaluationResult: PolicyEvaluationResult =
-        await opaClient.evaluatePolicy(input);
-  
-      return {
-        result: evaluationResult.deny
-          ? AuthorizeResult.DENY
-          : AuthorizeResult.ALLOW,
-      };
+        identity: {
+          username: userName,
+          groups: userGroups,
+        },
+      },
     };
-  }
+
+    const response = await opaClient.evaluatePolicy(input);
+    const allow = !response.deny; // If 'deny' is false, then 'allow' will be true
+    
+    // Returning the decision based on the 'allow' value
+    return {
+      result: allow ? AuthorizeResult.ALLOW : AuthorizeResult.DENY,
+    };
+  };
+}
