b84bc5d6f1ec0ffab01316d9cd5b7b6973c6f09b
Peter Macdonald <13601053+Parsifal-M@users.noreply.github.com>
2023-08-26 18:15:49 +0200

    Tidy opa wrapper (#42)
    * Fixing Isolated VM, Refactoring OPA Wrapper, tidied up un-needed files, updated backstage version.
    
    ---------
    
    Signed-off-by: Parsifal-M <macdonald.peter90@gmail.com>

--- plugins/opa-permissions-wrapper/src/opa-evaluator/opaPermissionEvaluator.ts
+++ plugins/opa-permissions-wrapper/src/opa-evaluator/opaPermissionEvaluator.ts
@@ -39,9 +39,6 @@ export const createOpaPermissionEvaluator = (opaClient: OpaClient) => {
       },
     };
 
-    // For debugging purposes
-    console.log('input', JSON.stringify(input, null, 2));
-
     const response: PolicyEvaluationResult = await opaClient.evaluatePolicy(
       input,
     );
