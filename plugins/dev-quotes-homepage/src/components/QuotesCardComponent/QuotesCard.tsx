6399d8c9da3a3a9decb0b6081498ca27a8d5b51d
Peter Macdonald <macdonald.peter90@gmail.com>
2023-10-14 14:49:44 +0200

    DevQuotes Card
    Signed-off-by: Peter Macdonald <macdonald.peter90@gmail.com>

--- plugins/dev-quotes-homepage/src/components/QuotesCardComponent/QuotesCard.tsx
+++ plugins/dev-quotes-homepage/src/components/QuotesCardComponent/QuotesCard.tsx
@@ -1,18 +1,17 @@
-import { Card, CardContent, Typography } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
+import { InfoCard } from '@backstage/core-components';
 import React from 'react';
 import { quotes } from '../../quotes';
 
-export const QuoteCard: React.FC = () => {
+export const QuoteCard = () => {
   const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
 
   return (
-    <Card>
-      <CardContent>
-        <Typography variant="h5" component="h2">
+    <InfoCard>
+        <Typography variant="h6">
           {randomQuote.text}
         </Typography>
-        <Typography color="textSecondary">{randomQuote.author}</Typography>
-      </CardContent>
-    </Card>
+        <Typography variant='subtitle2'>{randomQuote.author}</Typography>
+    </InfoCard>
   );
 };
