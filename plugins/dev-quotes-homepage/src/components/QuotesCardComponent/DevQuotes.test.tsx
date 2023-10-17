b468aa85756767b65498864c59e9300a80fe7098
Peter Macdonald <13601053+Parsifal-M@users.noreply.github.com>
2023-10-17 17:49:14 +0200

    Small Patch (#75)
    * Small Patch
    
    ---------
    
    Signed-off-by: Peter Macdonald <macdonald.peter90@gmail.com>

--- plugins/dev-quotes-homepage/src/components/QuotesCardComponent/DevQuotes.test.tsx
+++ plugins/dev-quotes-homepage/src/components/QuotesCardComponent/DevQuotes.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { DevQuoteCard } from './DevQuotesCard';
+import { DevQuote } from './DevQuotes';
 
 // Mock your quotes array for testing purposes
 jest.mock('../../quotes', () => ({
@@ -9,7 +9,7 @@ jest.mock('../../quotes', () => ({
 
 describe('QuoteCard', () => {
   it('renders a quote', () => {
-    const { getByText } = render(<DevQuoteCard />);
+    const { getByText } = render(<DevQuote />);
 
     // Assert that the quote text and author are displayed
     const quoteText = getByText('Sample quote text');
--- plugins/dev-quotes-homepage/src/components/QuotesCardComponent/DevQuotes.tsx
+++ plugins/dev-quotes-homepage/src/components/QuotesCardComponent/DevQuotes.tsx
@@ -1,15 +1,14 @@
 import { Typography } from '@material-ui/core';
-import { InfoCard } from '@backstage/core-components';
 import React from 'react';
 import { quotes } from '../../quotes';
 
-export const DevQuoteCard = () => {
+export const DevQuote = () => {
   const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
 
   return (
-    <InfoCard>
+    <>
       <Typography variant="h6">{randomQuote.text}</Typography>
       <Typography variant="subtitle2">{randomQuote.author}</Typography>
-    </InfoCard>
+    </>
   );
 };
