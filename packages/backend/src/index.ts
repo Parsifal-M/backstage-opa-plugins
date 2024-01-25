43a9de8fcee73a4c3d1dcdf8829c8aee6f6b3bac
StefanK13 <16716986+StefanK13@users.noreply.github.com>
2024-01-25 10:50:38 +0100

    Use the Auth plugin via the new backend system
    Signed-off-by: StefanK13 <16716986+StefanK13@users.noreply.github.com>

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -2,4 +2,6 @@ import { createBackend } from '@backstage/backend-defaults';
 
 const backend = createBackend();
 backend.add(import('@backstage/plugin-app-backend/alpha'));
+backend.add(import('@backstage/plugin-auth-backend'));
+backend.add(import('@backstage/plugin-auth-backend-module-github-provider'));
 backend.start();
