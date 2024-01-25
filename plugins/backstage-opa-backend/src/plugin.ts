78883c5fe47e391a52c2a374bee823ce4eaffd78
StefanK13 <16716986+StefanK13@users.noreply.github.com>
2024-01-25 15:06:18 +0100

    Migrate the plugin-opa-backend so it is also available on the new backend end system
    Signed-off-by: StefanK13 <16716986+StefanK13@users.noreply.github.com>

---
+++ plugins/backstage-opa-backend/src/plugin.ts
@@ -0,0 +1,24 @@
+import { loggerToWinstonLogger } from '@backstage/backend-common';
+import { coreServices, createBackendPlugin } from '@backstage/backend-plugin-api';
+import { createRouter } from './service/router';
+
+export const opaPlugin = createBackendPlugin({
+  pluginId: 'opa',
+  register(env) {
+    env.registerInit({
+      deps: {
+        config: coreServices.rootConfig,
+        logger: coreServices.logger,
+        httpRouter: coreServices.httpRouter,
+      },
+      async init({ logger, httpRouter, config }) {
+        httpRouter.use(
+          await createRouter({
+            config,
+            logger: loggerToWinstonLogger(logger),
+          }),
+        );
+      },
+    });
+  },
+});
