36fc9e7182835a2323ca18390bf8a379e710a7ec
StefanK13 <16716986+StefanK13@users.noreply.github.com>
2024-01-25 14:26:43 +0100

    Use the Catalog plugin via the new backend system
    Signed-off-by: StefanK13 <16716986+StefanK13@users.noreply.github.com>

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -4,4 +4,9 @@ const backend = createBackend();
 backend.add(import('@backstage/plugin-app-backend/alpha'));
 backend.add(import('@backstage/plugin-auth-backend'));
 backend.add(import('@backstage/plugin-auth-backend-module-github-provider'));
+backend.add(import('@backstage/plugin-catalog-backend/alpha'));
+backend.add(
+  import('@backstage/plugin-catalog-backend-module-scaffolder-entity-model'),
+);
+backend.add(import('@backstage/plugin-catalog-backend-module-github-org'));
 backend.start();
