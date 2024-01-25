551df2a773a73a694def07d6c31302897a44c770
StefanK13 <16716986+StefanK13@users.noreply.github.com>
2024-01-25 14:31:45 +0100

    Use the Techdocs plugin via the new backend system
    Signed-off-by: StefanK13 <16716986+StefanK13@users.noreply.github.com>

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -14,4 +14,5 @@ backend.add(import('@backstage/plugin-scaffolder-backend/alpha'));
 backend.add(import('@backstage/plugin-search-backend/alpha'));
 backend.add(import('@backstage/plugin-search-backend-module-catalog/alpha'));
 backend.add(import('@backstage/plugin-search-backend-module-techdocs/alpha'));
+backend.add(import('@backstage/plugin-techdocs-backend/alpha'));
 backend.start();
