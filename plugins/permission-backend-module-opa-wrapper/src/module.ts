dd2e37fca7f83ff382a2482992554ae0c03d8c91
Peter Macdonald <macdonald.peter90@gmail.com>
2024-03-14 14:31:02 +0100

    change to using correct logger, this allows for debug logs
    Signed-off-by: Peter Macdonald <macdonald.peter90@gmail.com>

--- plugins/permission-backend-module-opa-wrapper/src/module.ts
+++ plugins/permission-backend-module-opa-wrapper/src/module.ts
@@ -5,7 +5,6 @@ import {
 import { policyExtensionPoint } from '@backstage/plugin-permission-node/alpha';
 import { OpaPermissionPolicy } from './policy';
 import { OpaClient } from './opa-client';
-import { loggerToWinstonLogger } from '@backstage/backend-common';
 
 export const permissionModuleOpaWrapper = createBackendModule({
   pluginId: 'permission',
@@ -20,8 +19,8 @@ export const permissionModuleOpaWrapper = createBackendModule({
       async init({ config, logger, policy }) {
         policy.setPolicy(
           new OpaPermissionPolicy(
-            new OpaClient(config, loggerToWinstonLogger(logger)),
-            loggerToWinstonLogger(logger),
+            new OpaClient(config, logger),
+            logger
           ),
         );
       },
