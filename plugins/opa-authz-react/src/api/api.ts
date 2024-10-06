7a629cf1d43e60400ef1242336d7e2bec2708f05
Peter Macdonald <macdonald.peter90@gmail.com>
2024-10-06 16:24:43 +0100

    working on tests
    Signed-off-by: Peter Macdonald <macdonald.peter90@gmail.com>

--- plugins/opa-authz-react/src/api/api.ts
+++ plugins/opa-authz-react/src/api/api.ts
@@ -3,7 +3,7 @@ import { OpaAuthzApi, PolicyInput, PolicyResult } from "./types";
 
 
 export class OpaAuthzClientReact implements OpaAuthzApi {
-  private fetchApi: FetchApi;
+  private readonly fetchApi: FetchApi;
   constructor(options: { fetchApi: FetchApi }) {
     this.fetchApi = options.fetchApi;
   }
