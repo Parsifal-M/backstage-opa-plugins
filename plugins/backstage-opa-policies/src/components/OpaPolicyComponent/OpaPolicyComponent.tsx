e43fb2d7b8f0cc4b5bdf1938a1c3b201b2391702
Peter Macdonald <macdonald.peter90@gmail.com>
2024-11-10 15:01:44 +0100

    adds rego styles for both light and dark and a copy to clipboard button
    Signed-off-by: Peter Macdonald <macdonald.peter90@gmail.com>

--- plugins/backstage-opa-policies/src/components/OpaPolicyComponent/OpaPolicyComponent.tsx
+++ plugins/backstage-opa-policies/src/components/OpaPolicyComponent/OpaPolicyComponent.tsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { Content, InfoCard, Progress } from '@backstage/core-components';
+import { Content, InfoCard, Progress, CopyTextButton } from '@backstage/core-components';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { opaPolicyBackendApiRef, OpaPolicy } from '../../api/types';
 import { useApi, alertApiRef } from '@backstage/core-plugin-api';
 import { useEntity } from '@backstage/plugin-catalog-react';
-import { a11yDark, dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { a11yDark, coy } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { useTheme } from '@material-ui/core';
 
 export const OpaPolicyPage = () => {
+  const theme = useTheme();
   const [policy, setPolicy] = useState<OpaPolicy | null>(null);
   const [loading, setLoading] = useState(true);
   const opaApi = useApi(opaPolicyBackendApiRef);
@@ -44,13 +46,21 @@ export const OpaPolicyPage = () => {
       <InfoCard
         title={`${entity.metadata.name} OPA Policy`}
         data-testid="opa-policy-card"
+        action={
+          <CopyTextButton 
+            text={policy?.opaPolicyContent ?? ''} 
+            tooltipText="Copied policy to clipboard!"
+          />
+        }
       >
         <SyntaxHighlighter
           language="rego"
-          style={a11yDark}
+          style={theme.palette.type === 'dark' ? a11yDark : coy}
           customStyle={{ background: 'inherit', fontSize: '110%' }}
+          showLineNumbers
+          
         >
-          {policy?.opaPolicyContent}
+          {policy?.opaPolicyContent ?? ''}
         </SyntaxHighlighter>
       </InfoCard>
     </Content>
