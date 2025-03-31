4d4ecf84f2974fc36e54adaef9fa79a436570e85
Peter Macdonald <peterm4c@pm.me>
2025-03-31 21:56:21 +0100

    no blog for now
    Signed-off-by: Peter Macdonald <peterm4c@pm.me>

--- opa-docs/docusaurus.config.ts
+++ opa-docs/docusaurus.config.ts
@@ -114,10 +114,6 @@ const config: Config = {
           title: 'More',
           items: [
             {
-              label: 'Blog',
-              to: '/blog',
-            },
-            {
               label: 'GitHub',
               href: 'https://github.com/Parsifal-M/backstage-opa-plugins',
             },
