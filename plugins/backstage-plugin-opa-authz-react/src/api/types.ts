244f3fa41613764f08d716d279a37c90c0f47119
Peter Macdonald <peterm4c@pm.me>
2026-03-17 15:27:20 +0100

    dont re-export the policyresult from the opa authz react, instead consumers should use the common package
    Signed-off-by: Peter Macdonald <peterm4c@pm.me>

--- plugins/backstage-plugin-opa-authz-react/src/api/types.ts
+++ plugins/backstage-plugin-opa-authz-react/src/api/types.ts
@@ -3,8 +3,6 @@ import {
   PolicyResult,
 } from '@parsifal-m/backstage-plugin-opa-common';
 
-export type { PolicyResult };
-
 export type OpaAuthzApi = {
   evalPolicy(input: PolicyInput, entryPoint: string): Promise<PolicyResult>;
 };
